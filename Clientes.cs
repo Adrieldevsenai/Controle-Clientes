555dd1a80ccda2190981cb0b3fd72056ee7eee05
Adriel <adrieloliveira10@gmail.com>
2024-11-03 21:56:07 -0300

    controle clientes

---
+++ Clientes.cs
@@ -0,0 +1,20 @@
+
+
+namespace Controle_Clientes {
+
+    public class Clientes {
+        public string nome {get; set;}
+        public string endereco { get; set; }
+        public float valor { get; protected set; }
+        public float valorImposto { get; protected set; }    
+        public float total { get; set; }
+
+        public virtual void Pagar_Imposto(float v) {
+            valor = v;
+            valorImposto = valor * 10/100;
+            total = valor + valorImposto;
+
+        }
+
+    }
+}
---
+++ Pessoafisica.cs
@@ -0,0 +1,13 @@
+
+
+using Controle_Clientes;
+
+namespace Controle_clientes {
+
+    public class Pessoafisica : Clientes {
+        public string cpf {get; set;}
+        public string rg {get; set;}
+
+
+    }
+}
